94956d0b7cab761db57a3e749cfcba73db56e42d
Andrew Palmer <andrew.pa@outlook.com>
2017-07-21 16:28:17 -0700

    vulkan pipeline wrapper

---
+++ vkng/pipeline.cpp
@@ -0,0 +1,24 @@
+#include "pipeline.h"
+
+namespace vkng {
+	pipeline::pipeline(device* d, vk::PipelineLayout layout, vk::RenderPass renderpass, 
+		uint32_t subpass, options b, vk::Pipeline basepipe, uint32_t baseindex) {
+
+		vk::PipelineViewportStateCreateInfo vp;
+		vp.pScissors = &b.scissor; vp.pViewports = &b.viewport;
+		vp.viewportCount = vp.scissorCount = 1;
+		vk::GraphicsPipelineCreateInfo ifo {
+			vk::PipelineCreateFlags(), b.shaders.size(), b.shaders.data(),
+			&b.vi, &b.ia, nullptr, &vp, &b.rs, &b.multisample_ifo, nullptr, &b.blend,
+			b.dynamic_states_.size() > 0 ? 
+				&vk::PipelineDynamicStateCreateInfo(vk::PipelineDynamicStateCreateFlags(), 
+					b.dynamic_states_.size(), b.dynamic_states_.data()) 
+				: nullptr,
+			layout, renderpass, subpass, basepipe, baseindex 
+		};
+		
+		pp = d->dev->createGraphicsPipelineUnique(VK_NULL_HANDLE, ifo);
+	}
+	pipeline::~pipeline() {
+	}
+}
---
+++ vkng/pipeline.h
@@ -0,0 +1,94 @@
+﻿#pragma once
+#include "cmmn.h"
+#include "device.h"
+
+namespace vkng {
+
+	class shader_cashe {
+		map<string, vk::UniqueShaderModule> shaders;
+		device* dev; //this device must live at least as long as the shader_cache, RIP no Rust 😟
+	public:
+		shader_cashe(device* dev) : dev(dev) {}
+
+		vk::ShaderModule load_shader(const string& path) {
+			auto f = shaders.find(path);
+			if (f != shaders.end()) return f->second.get();
+			else {
+				ifstream file(path, ios::ate | ios::binary);
+				vector<char> buffer((size_t)file.tellg());
+				file.seekg(0);
+				file.read(buffer.data(), buffer.size());
+				file.close();
+				vk::ShaderModuleCreateInfo cfo;
+				cfo.codeSize = buffer.size();
+				cfo.pCode = (uint32_t*)buffer.data();
+				shaders[path] = dev->dev->createShaderModuleUnique(cfo);
+				return shaders[path].get();
+			}
+		}
+	};
+
+	struct pipeline {
+		struct options {
+			vk::PipelineVertexInputStateCreateInfo vi;
+			vk::PipelineInputAssemblyStateCreateInfo ia;
+			vk::Viewport viewport;
+			vk::Rect2D scissor;
+			vk::PipelineRasterizationStateCreateInfo rs;
+			vk::PipelineMultisampleStateCreateInfo multisample_ifo;
+			vector<vk::PipelineColorBlendAttachmentState> blend_states;
+			vk::PipelineColorBlendStateCreateInfo blend;
+			vector<vk::DynamicState> dynamic_states_;
+			vector<vk::PipelineShaderStageCreateInfo> shaders;
+
+			options() {}
+
+			options& viewport(vec2 viewport_size) {
+				viewport = vk::Viewport(0.f, 0.f, viewport_size.x, viewport_size.y, 0.f, 1.f);
+				scissor = vk::Rect2D(vk::Offset2D(), vk::Extent2D(viewport_size.x, viewport_size.y));
+			}
+			options& input_assembly(vk::PrimitiveTopology topology, bool prim_restart = false) {
+				ia.topology = topology;
+				ia.primitiveRestartEnable = prim_restart;
+				return *this;
+			}
+			options& viewport(vk::Viewport vp) { viewport = vp; return *this; }
+			options& rasterizer(vk::PolygonMode poly_mode = vk::PolygonMode::eFill,
+					vk::CullModeFlags cull_mode = vk::CullModeFlags(vk::CullModeFlagBits::eBack), vk::FrontFace ff = vk::FrontFace::eClockwise) {
+				rs.polygonMode = poly_mode;
+				rs.cullMode = cull_mode;
+				rs.frontFace = ff;
+				return *this;
+			}
+			options& rasterizer(vk::PipelineRasterizationStateCreateInfo ifo) { rs = ifo; return *this; }
+			options& blend(vk::PipelineColorBlendStateCreateInfo ifo, vector<vk::PipelineColorBlendAttachmentState> states) {
+				blend = ifo;
+				blend_states = states;
+				blend.attachmentCount = blend_states.size();
+				blend.pAttachments = blend_states.data();
+				return *this;
+			}
+			options& dynamic_states(vector<vk::DynamicState> states) {
+				dynamic_states_ = states;
+				return *this;
+			}
+
+			options& shader(vk::ShaderModule mod, vk::ShaderStageFlagBits stage, const string& name = "main") {
+				shaders.push_back(vk::PipelineShaderStageCreateInfo {
+					vk::PipelineShaderStageCreateFlags(), stage, mod, name.c_str() });
+				return *this;
+			}
+			options& vertex_shader(vk::ShaderModule mod, const string& name = "main") {
+				return shader(mod, vk::ShaderStageFlagBits::eVertex);
+			}
+			options& fragment_shader(vk::ShaderModule mod, const string& name = "main") {
+				return shader(mod, vk::ShaderStageFlagBits::eFragment);
+			}
+		};
+		vk::UniquePipeline pp;
+		pipeline(device* d, vk::PipelineLayout layout, vk::RenderPass renderpass, uint32_t subpass, 
+			options b, 
+			vk::Pipeline basepipe = VK_NULL_HANDLE, uint32_t baseindex = 0);
+		~pipeline();
+	};
+}
